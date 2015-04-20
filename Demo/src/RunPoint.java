1b767a62ac39697e8bc4d8e6fa9300cb469f69db
merbach <erbach@fh-aachen.de>
2015-04-20 14:40:09 +0200

    Changed RunPoint welcome message

--- Demo/src/RunPoint.java
+++ Demo/src/RunPoint.java
@@ -3,6 +3,8 @@ public class RunPoint {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Testing Git for lecture.");
+		
 		//EntryPoint ep = new EntryPoint();
 		ChildPoint cp = new ChildPoint();
 		
