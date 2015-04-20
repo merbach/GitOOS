7bbdd638128d66b6611e13ec3f22d870c7c27161
merbach <erbach@fh-aachen.de>
2015-04-20 16:56:02 +0200

    Changes for different branch

--- Demo/src/RunPoint.java
+++ Demo/src/RunPoint.java
@@ -4,6 +4,7 @@ public class RunPoint {
 	public static void main(String[] args) {
 		
 		System.out.println("Testing Git for lecture. Merge feature. Beep");
+		System.out.println("Loading Branch Plan_BB. This is Batman approved.");
 		
 		//EntryPoint ep = new EntryPoint();
 		ChildPoint cp = new ChildPoint();
